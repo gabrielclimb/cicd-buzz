efd0f1b7b56c8505badc70a331d839a7593689d4
gabrielclimb <gabrielclimbing@hotmail.com>
2019-01-08 00:13:54 -0200

    changes

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,8 +1,8 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
+buzz = ('continuous testing', 'continuous integration','continuous deployment',
+ 'continuous improvement', 'devops', 'geologist', 'FATEC is CETAF')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
     'seriously')
